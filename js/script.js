30f0f47070a08055cce99f730abb348ad1b7fe77
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-18 12:09:42 +0100

    v-for contacts printing + styling container

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-/*
-Milestone 1
-1- Visualizzazione dinamica della lista contatti: tramite la direttiva v-for,
- visualizzare nome e immagine di ogni contatto
-
-2- Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi)
- e dall’interlocutore (bianco) assegnando due classi CSS diverse
-*/
-
 new Vue({
     el: '#app',
     data: {
